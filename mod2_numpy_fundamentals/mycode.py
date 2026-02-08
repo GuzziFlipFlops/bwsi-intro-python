0c71ba577000afb5c7bbbd7ea8fe1ce8d8a23432
Aditya <95488593+GuzziFlipFlops@users.noreply.github.com>
2026-02-07 20:57:57 -0500

    e

--- mod2_numpy_fundamentals/mycode.py
+++ mod2_numpy_fundamentals/mycode.py
@@ -2,4 +2,7 @@ import numpy as np
 
 
 my_array = np.array([[12, 73, 68], [33, 25, 1], [54, 46, 92]])
-print(my_array)
+
+
+a = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+print(a[a==5])
